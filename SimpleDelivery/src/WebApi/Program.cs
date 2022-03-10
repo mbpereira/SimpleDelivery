4e4124a7ab4dddb45ff02aad3b9415804797922b
= <=>
2022-03-10 07:27:53 -0400

    Atualizando estrutura de pastas do projeto; removendo projetos de testes da publicação e da compilação; atualizando dockerfile e criando docker-compose.

--- SimpleDelivery/src/WebApi/Program.cs
+++ SimpleDelivery/src/WebApi/Program.cs
@@ -1,11 +1,9 @@
+using Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApi
 {
@@ -13,7 +11,16 @@ namespace WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<SimpleDeliveryContext>();
+                if(db.Database.GetPendingMigrations().Any())
+                    db.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
